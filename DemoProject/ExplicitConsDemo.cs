5b5671582a79e6060460344fd7f7e8c223cf1f5b
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-09 05:52:56 +0100

    Add project files.

---
+++ DemoProject/ExplicitConsDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    internal class ExplicitConsDemo
+    {
+        public ExplicitConsDemo()
+        {
+            Console.WriteLine("Constructor is called");
+        }
+        static void Main()
+        {
+            ExplicitConsDemo obj1 = new ExplicitConsDemo();
+            ExplicitConsDemo obj2 = new ExplicitConsDemo();
+            ExplicitConsDemo obj3 = new ExplicitConsDemo();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ DemoProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoProject
+{
+    class Program
+    {
+        int i; bool b;
+        static void Main(string[] args)
+        {
+            Program p = new DemoProject.Program();
+            Console.WriteLine("Value of i is: " + p.i);
+            Console.WriteLine("Value of p is: " + p.b);
+
+            Console.ReadLine();
+        }
+    }
+}
