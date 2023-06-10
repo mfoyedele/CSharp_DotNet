318d27f8656b2cd7d7b404b9274a06da11c98297
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-10 11:03:57 +0100

    Parameterized constructor added

--- DemoProject/ParameterizedConDemo.cs
+++ DemoProject/ParameterizedConDemo.cs
@@ -4,19 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DemoProject
 {
     internal class ParameterizedConDemo
     {
+       int x;
         public ParameterizedConDemo(int i)
         {
+            x = i;
+
             Console.WriteLine("Parameterized Constructor called is " + i);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Value of x is " + x);
         }
 
         static void Main()
         {
             ParameterizedConDemo num1 = new ParameterizedConDemo(10);
             ParameterizedConDemo num2 = new ParameterizedConDemo(20);
+
+            num1.Display();
+            num2.Display();
+            Console.ReadLine();
         }
     }
 }
