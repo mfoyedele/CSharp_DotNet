9c2a75e5812ada2978c894c84aa54aec7f54581d
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-11 11:43:10 +0100

    Second class values explicitly added

--- DemoProject/First.cs
+++ DemoProject/First.cs
@@ -14,6 +14,11 @@ namespace DemoProject
     class Second
     {
         public int x;
+
+        public Second(int x)
+        {
+            this.x = x;
+        }
     }
     class TestClasses
     {
@@ -24,9 +29,9 @@ namespace DemoProject
             First f3 = new First();
             Console.WriteLine(f1.x + " " + f2.x + " " + f3.x);
             
-            Second s1 = new Second();
-            Second s2 = new Second();
-            Second s3 = new Second();
+            Second s1 = new Second(100);
+            Second s2 = new Second(200);
+            Second s3 = new Second(300);
             Console.WriteLine(s1.x + " " + s2.x + " " + s3.x);
             Console.ReadLine();
         }
