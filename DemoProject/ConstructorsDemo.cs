755c239fddff10641de75c94bac9c78308d6a4fe
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-11 13:11:20 +0100

    Values added to non static constructor

--- DemoProject/ConstructorsDemo.cs
+++ DemoProject/ConstructorsDemo.cs
@@ -14,17 +14,18 @@ namespace DemoProject
         {
             Console.WriteLine("Static constructor is called.");
         }
-        public ConstructorsDemo()
+        public ConstructorsDemo( int x)
         {
+            this.x = x;
             Console.WriteLine("Non-Static constructor is called.");
         }
         static void Main()
         {
             Console.WriteLine("Main method is called.");
 
-            ConstructorsDemo d1 = new ConstructorsDemo();
-            ConstructorsDemo d2 = new ConstructorsDemo();
-            ConstructorsDemo d3 = new ConstructorsDemo();
+            ConstructorsDemo d1 = new ConstructorsDemo(10);
+            ConstructorsDemo d2 = new ConstructorsDemo(20);
+            ConstructorsDemo d3 = new ConstructorsDemo(30);
 
             Console.WriteLine(y);
             Console.WriteLine(d1.x + " " + d2.x + " " + d3.x);
