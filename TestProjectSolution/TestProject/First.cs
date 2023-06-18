9a0bcf9c0e5d67514b478f622716c513dbf32714
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-18 11:24:52 +0100

    Differences between variable, instance and reference

--- TestProjectSolution/TestProject/First.cs
+++ TestProjectSolution/TestProject/First.cs
@@ -8,9 +8,12 @@ namespace TestProject
         static void Main(string[] args)
         {
             //First f; // Variable of a class is a copy of the class that is not initialized
-            //f = new First(); // Instance of a class is a copy of the class that is initialized by using the "new" keyword
+            //f = new First(); // Instance of a class is a copy of the class that is initialized by using the "new" keyword which has its own memory and never shared with another instance.
             //Console.WriteLine(f.x);
 
+            // Reference of a class is a copy of the class that is initialized by using an existing instance and references of class will not
+            // have any memory allocation they will be sharing the same memory of the instance that assigned for initializing the variable.
+            // Reference of a class can be called as a pointer to the instance and every modification we perform on the members using instance reflects when we access those members through reference and vice-versa.
             First f1 = new First(); // f1 is instance of class
             First f2 = f1; // f2 is reference of the class
             Console.WriteLine(f1.x + " " + f2.x);
