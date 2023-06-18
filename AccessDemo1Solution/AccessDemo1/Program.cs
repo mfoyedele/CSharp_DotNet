34bee7077501bafe2002e6eada13451a85d44287
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-18 11:45:54 +0100

    Access specificiers initialized

--- AccessDemo1Solution/AccessDemo1/Program.cs
+++ AccessDemo1Solution/AccessDemo1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AccessDemo1
+{
+    public class Program
+    {        
+        private void Test1()
+        {
+            Console.WriteLine("Private Method.");
+        }
+        internal void Test2()
+        {
+            Console.WriteLine("Internal Method.");
+        }
+        protected void Test3()
+        {
+            Console.WriteLine("Protected Method.");
+        }
+        protected internal void Test4()
+        {
+            Console.WriteLine("Protected Internal Method.");
+        }
+        public void Test5()
+        {
+            Console.WriteLine("Public Method.");
+        }
+        static void Main(string[] args)
+        { 
+            
+
+        }
+    }
+}
+
