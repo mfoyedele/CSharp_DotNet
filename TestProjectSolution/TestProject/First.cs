f835f4e570bae76820f5c27ddb4922983e73bc5f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-18 11:13:05 +0100

    Reference of a class update

--- TestProjectSolution/TestProject/First.cs
+++ TestProjectSolution/TestProject/First.cs
@@ -14,10 +14,10 @@ namespace TestProject
             First f1 = new First(); // f1 is instance of class
             First f2 = f1; // f2 is reference of the class
             Console.WriteLine(f1.x + " " + f2.x);
-            //f1.x = 200;
-            //Console.WriteLine(f1.x + " " + f2.x);
-            //f2.x = 300;
-            //Console.WriteLine(f1.x + " " + f2.x);
+            f1.x = 200;
+            Console.WriteLine(f1.x + " " + f2.x);
+            f2.x = 300;
+            Console.WriteLine(f1.x + " " + f2.x);
             Console.ReadLine();
 
         }
