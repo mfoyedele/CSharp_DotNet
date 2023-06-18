905ed3c4ad8a574e9e3ce2cad07fdaab81d7e4f6
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-18 10:57:22 +0100

    Two instances of f1 and f2 created

--- TestProjectSolution/TestProject/First.cs
+++ TestProjectSolution/TestProject/First.cs
@@ -7,11 +7,16 @@ namespace TestProject
         int x = 100;
         static void Main(string[] args)
         {
-            First f;
-            f = new First();
-            Console.WriteLine(f.x);
+            //First f; // Variable of a class is a copy of the class that is not initialized
+            //f = new First(); // Instance of a class is a copy of the class that is initialized by using the "new" keyword
+            //Console.WriteLine(f.x);
+
+            First f1 = new First(); // f1 is instance of class
+            First f2 = new First(); // f2 is instance of class
+            Console.WriteLine(f1.x + " " + f2.x);
             Console.ReadLine();
 
         }
     }
 }  
+
