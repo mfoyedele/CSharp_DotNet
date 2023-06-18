29e5f4553a66a36db6f035e86aa4dafd452558c2
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-18 15:12:06 +0100

    Add project 1 reference solution into project 2

--- AccessDemo1Solution/AccessDemo2/Four.cs
+++ AccessDemo1Solution/AccessDemo2/Four.cs
@@ -2,7 +2,8 @@
 
 namespace AccessDemo2
 {
-    public class Four
+    // Case 2: Consuming members of a class from child class of different project
+    class Four : AccessDemo1.Program
     {
         static void Main(string[] args)
         {
