2f885701502b71b79ce1d39b86a17d1c888192a0
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-21 17:13:03 +0100

    Readonly variable update

--- TypesProject/TypesProject/Program.cs
+++ TypesProject/TypesProject/Program.cs
@@ -8,17 +8,17 @@ namespace TypesProject
         static int y = 200; //Static variable
         const float pi = 3.14f; // Constatic variable
         readonly bool flag;
-        public Program(int x)
+        public Program(int x, bool flag)
         {
             this.x = x;
-            this.flag = true;
+            this.flag = flag;
         }
         static void Main(string[] args)
         {
             Console.WriteLine(y);
             Console.WriteLine(pi);
-            Program p1 = new Program(15);
-            Program p2 = new Program(50);
+            Program p1 = new Program(15, true);
+            Program p2 = new Program(50, false);
             Console.WriteLine(p1.x + " " + p2.x);            
             Console.ReadLine();
         }
