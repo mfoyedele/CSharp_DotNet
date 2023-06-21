0dbcaeebd7e11549c3e3f12e8debcb41551d0d26
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-21 16:49:44 +0100

    Two instances intialized

--- TypesProject/TypesProject/Program.cs
+++ TypesProject/TypesProject/Program.cs
@@ -6,13 +6,17 @@ namespace TypesProject
     {
         int x = 100; //Non-Static variable
         static int y = 200; //Static variable
+
+        public Program(int x)
+        {
+            this.x = x;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine(y);
-            Program p1 = new Program();
-            Program p2 = new Program();
-            Console.WriteLine(p1.x);
-            Console.WriteLine(p2.x);
+            Program p1 = new Program(15);
+            Program p2 = new Program(50);
+            Console.WriteLine(p1.x + " " + p2.x);            
             Console.ReadLine();
         }
     }
