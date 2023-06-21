970d1df6b9cdf830dca9333d9270843c7cc99b74
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-06-21 16:29:01 +0100

    Non-static and static variables initialized

--- TypesProject/TypesProject/Program.cs
+++ TypesProject/TypesProject/Program.cs
@@ -4,9 +4,12 @@ namespace TypesProject
 {
     class Program
     {
+        int x = 100; //Non-Static variable
+        static int y = 200; //Static variable
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(y);
+            Console.ReadLine();
         }
     }
 }
