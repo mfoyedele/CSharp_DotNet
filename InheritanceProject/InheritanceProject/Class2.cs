e99f58b0006eb10b4c3938b16b0ecb6011b448b3
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-01 16:16:53 +0100

    Class2 inherit properties of class1

---
+++ InheritanceProject/InheritanceProject/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    internal class Class2 : Class1
+    {
+        static void Main(string[] args)
+        {
+            Class2 c = new Class2();
+            c.Test1();
+            c.Test2();
+            Console.ReadLine();
+        }
+    }
+}
