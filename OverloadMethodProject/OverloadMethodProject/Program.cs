96a7d3f48ef47d4c6878ac2ee32e5dfb37dd383f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-12 18:59:16 +0100

    Overloading method types list

---
+++ OverloadMethodProject/OverloadMethodProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OverloadMethodProject
+{
+    class Program
+    {      
+        public void Test()
+        {
+            Console.WriteLine("1st Method.");
+        }
+        public void Test(int i)
+        {
+            Console.WriteLine("2nd Method.");
+        }
+        public void Test(string s)
+        {
+            Console.WriteLine("3rd Method.");
+        }
+        public void Test(int i, string s)
+        {
+            Console.WriteLine("4th Method.");
+        }
+        public void Test(string s, int i)
+        {
+            Console.WriteLine("5th Method.");
+        }
+        static void Main(string[] args)
+        {
+          
+        }       
+
+    }
+}
