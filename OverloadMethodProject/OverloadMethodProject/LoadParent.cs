926a932249580e27d3bbec947ccf9b7b67f9d596
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-15 01:11:35 +0100

    Polymorphism method overloading class initiated between parent class and child class

---
+++ OverloadMethodProject/OverloadMethodProject/LoadParent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodProject
+{
+    internal class LoadParent
+    {
+        public void Show()
+        {
+            Console.WriteLine("Parent's Show Method is Callled.");
+        }
+        public void Test()
+        {
+            Console.WriteLine("Parent's Test Method is Called.");
+        }
+    }
+
+    internal class LoadChild : LoadParent
+    {
+        static void Main(string[] args) 
+        {
+            LoadParent c = new LoadParent();
+            c.Show();
+            c.Test();
+        }
+
+    }
+}
