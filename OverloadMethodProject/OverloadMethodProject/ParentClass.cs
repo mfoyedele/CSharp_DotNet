fd99a71413925d8900cb7a10fa60c19199008447
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-15 02:02:48 +0100

    Method hiding/shadowing initiated

---
+++ OverloadMethodProject/OverloadMethodProject/ParentClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OverloadMethodProject
+{
+    internal class ParentClass
+    {
+        public virtual void Test1()
+        {
+            Console.WriteLine("Method Test1 from parent class.");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method Test2 from parent class.");
+        }
+    }
+
+    internal class ChildClass : ParentClass
+    {
+        static void Main()
+        {
+            ChildClass c = new ChildClass();
+            c.Test1();
+            c.Test2();
+        }
+    }
+}
