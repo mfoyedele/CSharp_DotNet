c7c6378c55beffa201425d78825c0aec981b8068
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-19 16:34:54 +0100

    Matrix table initiated

---
+++ OverloadMethodProject/OverloadMethodProject/Matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodProject
+{
+    internal class Matrix
+    {
+        int a, b, c, d;
+        public Matrix(int a, int b, int c, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+    }
+}
