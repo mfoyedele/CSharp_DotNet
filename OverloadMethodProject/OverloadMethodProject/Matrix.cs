2834f0f7556034f4559059723a9178d4ff2ef3b7
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-19 17:13:56 +0100

    update matrix class

--- OverloadMethodProject/OverloadMethodProject/Matrix.cs
+++ OverloadMethodProject/OverloadMethodProject/Matrix.cs
@@ -21,6 +21,12 @@ namespace OverloadMethodProject
             Matrix obj = new Matrix(obj1.a +obj2.a, obj1.b+obj2.b, obj1.c+obj2.c, obj1.d+obj2.d);
             return obj;
         }
+
+        public static Matrix operator -(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a - obj2.a, obj1.b - obj2.b, obj1.c - obj2.c, obj1.d - obj2.d);
+            return obj;
+        }
     }
     class TestMatrix
     {
@@ -29,6 +35,14 @@ namespace OverloadMethodProject
             Matrix m1 = new Matrix(20, 18, 16, 14);
             Matrix m2 = new Matrix(10, 8, 6, 4);
             Matrix m3 = m1 + m2;
+            Matrix m4 = m1 - m2;
+
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+            Console.WriteLine(m4);
+
+            Console.ReadLine();
         }
     }
 }
