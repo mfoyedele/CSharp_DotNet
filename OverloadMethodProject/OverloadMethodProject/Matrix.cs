4e7c70e242331083bd781d4f2edca16e49debd6b
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-19 16:56:33 +0100

    Modifier operator added

--- OverloadMethodProject/OverloadMethodProject/Matrix.cs
+++ OverloadMethodProject/OverloadMethodProject/Matrix.cs
@@ -16,6 +16,11 @@ namespace OverloadMethodProject
             this.c = c;
             this.d = d;
         }
+        public static Matrix operator +(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a +obj2.a, obj1.b+obj2.b, obj1.c+obj2.c, obj1.d+obj2.d);
+            return obj;
+        }
     }
     class TestMatrix
     {
@@ -23,6 +28,7 @@ namespace OverloadMethodProject
         {
             Matrix m1 = new Matrix(20, 18, 16, 14);
             Matrix m2 = new Matrix(10, 8, 6, 4);
+            Matrix m3 = m1 + m2;
         }
     }
 }
