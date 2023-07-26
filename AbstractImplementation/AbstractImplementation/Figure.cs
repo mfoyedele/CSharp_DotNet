19dfc3ed8ac54a6c244be7c7faff5589f900ca32
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-26 20:45:48 +0100

    Classes and their attributes declared

--- AbstractImplementation/AbstractImplementation/Figure.cs
+++ AbstractImplementation/AbstractImplementation/Figure.cs
@@ -8,4 +8,30 @@ namespace AbstractImplementation
         public const float Pi = 3.14f;
 
     }
+    public class Rectangle : Figure
+    {
+        public Rectangle(double Width, double Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+    }
+
+    public class Circle : Figure
+    {
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+    }
+
+    public class Cone : Figure
+    {
+        public Cone(double Radius, double Height)
+        {
+            this.Radius = Radius;
+            this.Height = Height;
+        }
+    }
+
 }
