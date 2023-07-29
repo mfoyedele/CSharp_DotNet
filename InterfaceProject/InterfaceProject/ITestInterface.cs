4e3bd56690ab7f40026d88a2902c807cddcabf16
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-07-29 20:26:42 +0100

    update interface

--- InterfaceProject/InterfaceProject/ITestInterface.cs
+++ InterfaceProject/InterfaceProject/ITestInterface.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace InterfaceProject
 {
-    internal interface ITestInterface
+    internal interface ITestInterface1
     {
         void Add(int a, int b);
+    }
+    interface ITestInterface2 : ITestInterface1
+    {
+        void Sub(int a, int b);
     }
 }
